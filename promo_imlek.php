deb8f44cab188c444035044670fa5da40b5a3c50
Muhammad Zulfikar <harranobu@gmail.com>
2022-02-01 22:26:30 +0800

    update promo imlek

---
+++ promo_imlek.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Promo Imlek 2022 - Global Integra Technology</title>
+    <meta name="description" content="Global Integra Technology" />
+    <meta name="keywords" content="" />
+
+    <link rel="icon" type="image/png" href="assets/images/favicon/favicon.png" />
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <link href="assets/vendor/sweetalert2/dist/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+</head>
+<style>
+    .title {
+        color: gold;
+        font-size: 45px;
+    }
+
+    .page {
+        background: url(assets/images/lunar_new_year/bg-01.png);
+        background-size: cover;
+        background-position: 100% -15px;
+        background-repeat: no-repeat;
+        margin-bottom: -15px;
+    }
+
+    .top-logo img {
+        width: 65%;
+        height: 100%;
+    }
+
+    .top-logo {
+        position: absolute;
+        top: -89px;
+        background: white;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 300px;
+        height: 150px;
+        border-radius: 50%;
+        text-align: center;
+        padding-top: 65px;
+    }
+
+    .left,
+    .right {
+        position: absolute;
+        width: 200px;
+    }
+
+    .left {
+        left: 5%;
+        top: 0px;
+    }
+
+    .right {
+        right: 5%;
+        top: 0px;
+    }
+
+    footer {
+        background: #500b14;
+        color: #ffbbbb !important;
+        font-weight: bolder;
+    }
+</style>
+
+<body>
+    <img class="left d-none d-md-block" src="assets/images/lunar_new_year/left.svg">
+    <img class="right d-none d-md-block" src="assets/images/lunar_new_year/right.svg">
+    <section class="page">
+        <div class="top-logo">
+            <img src="assets/images/lunar_new_year/hikvision_logo.svg">
+        </div>
+        <div class="page-bottom">
+            <div class="container">
+                <div class="row justify-content-center text-center mb-5">
+                    <div class="col-md-8">
+                        <h1 class="fw-bold title font-size-24 mt-2 mb-5">PROMO IMLEK 2022</h1>
+                        <img style="width: 500px; max-width: 80%;" src="assets/images/lunar_new_year/tiger.svg">
+                    </div>
+                </div>
+                <div class="row justify-content-center mb-4">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header"><b><i class="fa fa-fw fa-edit"></i> FORMULIR PROMO</b></div>
+                            <div class="card-body">
+                                <form id="form-promo" action="#" method="POST">
+                                    <div class="form-group mb-2">
+                                        <label for="nama">Nama Lengkap</label>
+                                        <input type="text" class="form-control" id="name" name="name" placeholder="Nama Lengkap">
+                                    </div>
+                                    <div class="form-group mb-2">
+                                        <label for="handphone">No. HP/WhatsApp</label>
+                                        <input type="text" class="form-control" id="handphone" name="handphone" placeholder="No. HP/WhatsApp">
+                                    </div>
+                                    <div class="form-group mb-2">
+                                        <label for="package">Paket Promo</label>
+                                        <select class="form-control" name="package">
+                                            <option value="1">4CH HIKVISION 2MP - Rp. 3.700.000</option>
+                                            <option value="1">8CH HIKVISION 2MP - Rp. 5.700.000</option>
+                                            <option value="1">16CH HIKVISION 2MP - Rp. 9.899.000</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group d-grid mt-4">
+                                        <button class="btn-primary-line" type="submit">Kirim <i class="ms-2 fa fa-fw fas fa-arrow-right"></i></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header"><b><i class="fa fa-fw fa-bookmark"></i> SYARAT DAN KETENTUAN</b></div>
+                            <div class="card-body">
+                                <ul>
+                                    <li>asd</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <img src="assets/images/lunar_new_year/bottom.svg"> -->
+    </section>
+
+    <a href="#" class="back-to-top" style="display: block;"><i class="fa fa-chevron-up"></i></a>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-center">Global Integra Technology</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/parallax.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script>
+    <script src="assets/vendor/sweetalert2/dist/sweetalert2.min.js"></script>
+
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
