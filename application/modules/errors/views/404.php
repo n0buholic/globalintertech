7f0c79d00bf8e35d7bd1590504268aaeb61ead9a
Muhammad Zulfikar <50941281+n0buholic@users.noreply.github.com>
2022-02-04 15:34:18 +0800

    halaman 404

--- application/modules/errors/views/404.php
+++ application/modules/errors/views/404.php
@@ -1,36 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <?php $this->load->view('templates/frontend_header') ?>
-</head>
-
-<body class="appear-animate">
-
-  <div class="page" id="top">
-    <main id="main">
-      <section class="home-section bg-dark-alfa-30 parallax-2" data-background="<?= base_url() ?>assets/themes/images/full-width-images/section-bg-17.jpg">
-        <div class="js-height-full container">
-          <div class="home-content">
-            <div class="home-text">
-              <h1 class="hs-line-8 font-alt mb-50 mb-xs-30">
-                <?= $site_name ?>
-              </h1>
-              <h2 class="hs-line-14 font-alt mb-50 mb-xs-30">
-                404 Tidak Ditemukan
-              </h2>
-              <div class="local-scroll">
-                <a href="<?= base_url() ?>" class="btn btn-mod btn-border-w btn-medium btn-round" tabindex="0">Kembali</a>
-              </div>
-            </div>
-          </div>
-
+<section class="page">
+  <div class="page-bottom">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 text-center">
+          <h1 class="display-1 fw-bold">404</h1>
+          <h5 class="title">Halaman tidak ditemukan</h5>
+          <p class="mt-3">Halaman yang Anda cari tidak ditemukan</p>
+          <a href="<?= base_url() ?>" class="btn-primary-line btn-mini">Kembali ke Halaman Utama <i class="ms-2 fa-fw fas fa-home"></i></a>
         </div>
-      </section>
-    </main>
+      </div>
+    </div>
   </div>
-
-  <?php $this->load->view('templates/frontend_js') ?>
-</body>
-
-</html>
+</section>
