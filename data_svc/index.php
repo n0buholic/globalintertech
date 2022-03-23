5bf66d84fe363727017f0fc6881609dc0d1cf7f8
rahmangit33 <burlok67@gmail.com>
2022-03-23 15:36:26 +0800

    edit data service index

--- data_svc/index.php
+++ data_svc/index.php
@@ -31,7 +31,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 	<script src=""></script>
-	<script type="text/javascript" src="../assets/js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="../assets/frontend/js/jquery-3.6.0.min.js"></script>
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js"></script>
 
 	<script type="text/javascript">
