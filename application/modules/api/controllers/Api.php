43f154c73a6ea137846ea99ec947f7465a5d593f
Robby <harranobu@gmail.com>
2022-05-14 11:15:39 +0800

    update

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -442,12 +442,13 @@ class Api extends MX_Controller
 			// get file
 			$url = base_url("sales-quote-preview/view?id=" . $sq->id);
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_REFERER, 'http://www.example.com/1');
-			curl_setopt($ch, CURLOPT_HEADER, 0);
-			curl_setopt($ch, CURLOPT_VERBOSE, 0);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
-			curl_setopt($ch, CURLOPT_URL, urlencode($url));
+			curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+			curl_setopt($ch, CURLOPT_HEADER, 1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($ch, CURLOPT_URL, $url);
 			$response = curl_exec($ch);
 			curl_close($ch);
 
